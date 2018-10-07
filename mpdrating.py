c8802209b2f85eb329cf5a919409caf39e3b7eed
makurell <makureoll@gmail.com>
2018-10-07 18:10:19 +0100

    impl: write playlists to disk

--- mpdrating.py
+++ mpdrating.py
@@ -40,12 +40,14 @@ class MPDConfig:
         return data
 
 if __name__ == '__main__':
-    music_dir = MPDConfig().parse().data['music_directory']
+    mpdconfig = MPDConfig().parse()
+    music_dir = mpdconfig.data['music_directory']
 
-    categories = ["BEST+", "Best", "Good",  "Ok+",   "Ok",  "Out",  "Bad","Unrated"]
-    bins =       [    248,    219,    191,    157,    123,     59,      1,        0]
+    categories = ["0 - BEST+", "1 - Best", "2 - Good",  "3 - Ok+",   "4 - Ok",  "5 - Out",  "6 - Bad","7 - Unrated"]
+    bins =       [        248,        219,        191,        157,        123,         59,          1,            0]
 
     playlists = {} # name: paths
+
     # make playlists from mp3s
     for root, _, filenames in os.walk(os.path.expanduser(music_dir)):
         for filename in fnmatch.filter(filenames, '*.mp3'):
@@ -62,4 +64,14 @@ if __name__ == '__main__':
                         playlist.append(song)
                         playlists[categories[i]] = playlist
                         break
-    print(playlists)
+            else:
+                playlist = playlists.get(categories[-1], [])
+                playlist.append(song)
+                playlists[categories[-1]] = playlist
+
+
+    # write playlists to mpd
+    for k,v in playlists.items():
+        with open(os.path.join(os.path.expanduser(mpdconfig.data['playlist_directory']),str(k)+'.m3u'),'w') as f:
+            for song in v:
+                f.write(song+'\n')

6b5a537b8947d9388a0bab3010fa7277895c8fe8
makurell <makureoll@gmail.com>
2018-10-07 17:41:59 +0100

    impl categorise based on POPM

--- mpdrating.py
+++ mpdrating.py
@@ -1,6 +1,8 @@
+import fnmatch
+import glob
 import os
 import re
-import mutagen
+import mutagen.id3
 
 class MPDConfig:
     def __init__(self,path=None):
@@ -43,4 +45,21 @@ if __name__ == '__main__':
     categories = ["BEST+", "Best", "Good",  "Ok+",   "Ok",  "Out",  "Bad","Unrated"]
     bins =       [    248,    219,    191,    157,    123,     59,      1,        0]
 
-    # commit existing playlists to Mp3
+    playlists = {} # name: paths
+    # make playlists from mp3s
+    for root, _, filenames in os.walk(os.path.expanduser(music_dir)):
+        for filename in fnmatch.filter(filenames, '*.mp3'):
+            fullpath = os.path.join(root, filename)
+            song = os.path.relpath(fullpath,music_dir)
+
+            audio = mutagen.id3.ID3(fullpath)
+            popms = audio.getall('POPM')
+            if len(popms)>0:
+                rating = popms[0].rating
+                for i in range(len(categories)):
+                    if rating>=bins[i]:
+                        playlist = playlists.get(categories[i],[])
+                        playlist.append(song)
+                        playlists[categories[i]] = playlist
+                        break
+    print(playlists)
